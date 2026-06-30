51f96cb8a01d25b349758176cb8f5387b8b0712f
Sahiil <sahil.xii.vii@gmail.com>
2026-06-30 10:23:47 +0530

    Create Notebook.jsx

---
+++ src/components/Notebook/Notebook.jsx
@@ -0,0 +1,13 @@
+function Notebook() {
+  return (
+    <section className="editor">
+      <h2>Welcome 👋</h2>
+
+      <p>
+        Nova Notebook v0.2 is running successfully.
+      </p>
+    </section>
+  );
+}
+
+export default Notebook;
