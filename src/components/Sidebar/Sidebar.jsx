281d1ff0a85a79557c481e2271200e0689a4b661
Sahiil <sahil.xii.vii@gmail.com>
2026-06-30 10:22:34 +0530

    Create Sidebar.jsx

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h3>Explorer</h3>
+
+      <button>+ New Notebook</button>
+
+      <ul>
+        <li>📓 Notebook 1</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
